dfa61549c7061e80c536c39532de1aa38a5c570a
Pasi Lallinaho <pasi@shimmerproject.org>
2012-01-28 23:58:04 +0200

    Small fixes.

--- xubuntu-wp/functions.php
+++ xubuntu-wp/functions.php
@@ -6,6 +6,14 @@ add_filter( 'widget_text', 'do_shortcode' );
 /* enable widget areas */
 if( function_exists( 'register_sidebar' ) ) {
 	register_sidebar( array(
+		"name" => "Frontpage",
+		"id" => "xubuntu_front",
+		"before_widget" => "<div class='column'>",
+		"after_widget" => "</div>",
+		"before_title" => "<h2>",
+		"after_title" => "</h2>"
+	) );
+	register_sidebar( array(
 		"name" => "Frontpage columns",
 		"id" => "xubuntu_front_columns",
 		"before_widget" => "<div class='column'>",
--- xubuntu-wp/sidebar.php
+++ xubuntu-wp/sidebar.php
@@ -3,8 +3,9 @@
 		if( function_exists( 'dynamic_sidebar' ) ) {
 			if( is_front_page( ) ) {
 				// print picslide here
-				echo do_shortcode( '[picslide size="450x450"]' );
+				// echo do_shortcode( '[photoslider size="medium"]' );
 				// dynamic_sidebar( "xubuntu_home_sidebar" );
+				dynamic_sidebar( "xubuntu_front" );
 			} else {
 				dynamic_sidebar( "xubuntu_sidebar" );
 			}
