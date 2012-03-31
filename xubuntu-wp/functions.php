476aa8736160089f4796dbbeab5fef510770dbec
Pasi Lallinaho <pasi@shimmerproject.org>
2012-03-31 22:51:28 +0300

    Clean up <head>

--- xubuntu-wp/functions.php
+++ xubuntu-wp/functions.php
@@ -46,4 +46,22 @@ function xubuntu_upload_mime( $mimes = array( ) ) {
 	return $mimes;
 }
 
+/* remove unnecessary stuff from head */
+remove_action( 'wp_head', 'feed_links_extra', 3 );
+remove_action( 'wp_head', 'feed_links', 2 );
+remove_action( 'wp_head', 'rsd_link' );
+remove_action( 'wp_head', 'wlwmanifest_link' );
+remove_action( 'wp_head', 'index_rel_link' );
+remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 );
+remove_action( 'wp_head', 'start_post_rel_link', 10, 0 );
+remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
+remove_action( 'wp_head', 'wp_generator' );
+
+/* and now add a meaningful feed link to the head */
+add_action( 'wp_head', 'xubuntu_feed_head' );
+function xubuntu_feed_head( ) {
+	// we always want to link to the main feed. always
+	print '<link rel="alternate" type="application/rss+xml" title="' . get_bloginfo( 'name' ) . ' // Articles feed" href="' . get_bloginfo( 'rss2_url' ) . '" />' . "\n";
+}
+
 ?>
