60088e492c998eb6104602d1c67236a996785d80
Pasi Lallinaho <pasi@shimmerproject.org>
2013-07-18 00:41:39 +0300

    Add editor styles.

--- xubuntu-wp/functions.php
+++ xubuntu-wp/functions.php
@@ -1,9 +1,15 @@
 <?php
 
-/* enable shortcodes for widgets */
+/*  Enable using shortcodes in widgets 
+ *
+ */
+
 add_filter( 'widget_text', 'do_shortcode' );
 
-/* enable widget areas */
+/*  Register widget areas
+ *
+ */
+
 if( function_exists( 'register_sidebar' ) ) {
 	register_sidebar( array(
 		"name" => "Frontpage",
@@ -39,14 +45,20 @@ if( function_exists( 'register_sidebar' ) ) {
 	) );
 }
 
-/* enable uploading svg's */
+/*  Allow uploading SVG
+ *
+ */
+
 add_filter( 'upload_mimes', 'xubuntu_upload_mime' );
 function xubuntu_upload_mime( $mimes = array( ) ) {
 	$mimes['svg'] = 'image/svg';
 	return $mimes;
 }
 
-/* remove unnecessary stuff from head */
+/*  Remove unwanted metadata from <head>
+ *
+ */
+
 remove_action( 'wp_head', 'feed_links_extra', 3 );
 remove_action( 'wp_head', 'feed_links', 2 );
 remove_action( 'wp_head', 'rsd_link' );
@@ -57,11 +69,23 @@ remove_action( 'wp_head', 'start_post_rel_link', 10, 0 );
 remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
 remove_action( 'wp_head', 'wp_generator' );
 
-/* and now add a meaningful feed link to the head */
+/*  Add RSS2 feed link to <head>
+ *
+ */
+
 add_action( 'wp_head', 'xubuntu_feed_head' );
 function xubuntu_feed_head( ) {
 	// we always want to link to the main feed. always
 	print '<link rel="alternate" type="application/rss+xml" title="' . get_bloginfo( 'name' ) . ' // Articles feed" href="' . get_bloginfo( 'rss2_url' ) . '" />' . "\n";
+}
+
+/*  Add an editor style
+ *
+ */
+
+add_action( 'init', 'xubuntu_editor_style' );
+function xubuntu_editor_style( ) {
+	add_editor_style( 'editor.css' );
 }
 
 ?>
