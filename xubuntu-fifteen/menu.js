73101561aed4dd28837b3926ec421871e314fae4
Pasi Lallinaho <pasi@shimmerproject.org>
2015-02-23 15:00:30 +0200

    Center the dropdown submenus relative to the main items when possible

--- xubuntu-fifteen/menu.js
+++ xubuntu-fifteen/menu.js
@@ -1,4 +1,24 @@
 jQuery( function( ) {
+	// DROPDOWN MENU
+
+	// Make sure submenus appear centered related to the main item when possible
+	jQuery( '#navi .menu > li' ).each( function( e ) {
+		var shift = ( jQuery( this ).children( '.sub-menu' ).outerWidth( ) - jQuery( this ).outerWidth( ) ) / 2;
+		var right_border = jQuery( this ).offset( ).left + jQuery( this ).children( '.sub-menu' ).outerWidth( ) - shift;
+
+		if( right_border > jQuery( window ).outerWidth( ) - 15 ) {
+			// Submenu would overflow, we need to add offset
+			var extra_offset = right_border - jQuery( window ).outerWidth( ) + 15 + shift;
+			jQuery( this ).children( '.sub-menu' ).css( 'left', -extra_offset);
+		} else if( jQuery( this ).offset( ).left - shift > 0 ) {
+			// The submenu will fit the screen
+			jQuery( this ).children( '.sub-menu' ).css( 'left', -shift );
+		}
+	} );
+
+
+	// MOBILE MENU
+
 	// Toggle CSS class on menu opening/closing
 	jQuery( '#opennavi' ).click( function( e ) {
 		jQuery( '#navi' ).toggleClass( 'open' );
