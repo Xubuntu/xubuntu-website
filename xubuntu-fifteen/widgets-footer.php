f121144d9aebcbf8ce550d381d084f1406db15a0
Pasi Lallinaho <pasi@shimmerproject.org>
2015-06-11 23:39:33 +0300

    Don't print a separator line above footer navigation if the first footer widget area is empty

--- xubuntu-fifteen/widgets-footer.php
+++ xubuntu-fifteen/widgets-footer.php
@@ -1,16 +1,18 @@
 <?php
 	if( function_exists( 'dynamic_sidebar' ) ) {
-		?>
-		<div id="footer-widgets" class="widgets_flex"><div class="widgets group"><?php dynamic_sidebar( 'footer-widgets' ); ?></div></div>
-		<?php
-			wp_nav_menu( array(
-				'theme_location' => 'footer_navigation',
-				'container_id' => 'footer_navi',
-				'container_class' => 'group navigation nh',
-				'fallback_cb' => false
-			) );
-		?>
-		<div id="footer-widgets-2" class="widgets group"><?php dynamic_sidebar( 'footer-widgets-2' ); ?></div>
-		<?php
+		if( is_active_sidebar( 'footer-widgets' ) ) {
+			?><div id="footer_widgets" class="widgets_flex"><div class="widgets group"><?php dynamic_sidebar( 'footer-widgets' ); ?></div></div><?php
+		}
+
+		wp_nav_menu( array(
+			'theme_location' => 'footer_navigation',
+			'container_id' => 'footer_navi',
+			'container_class' => 'group navigation nh',
+			'fallback_cb' => false
+		) );
+
+		if( is_active_sidebar( 'footer-widgets-2' ) ) {
+			?><div id="footer_widgets_2" class="widgets group"><?php dynamic_sidebar( 'footer-widgets-2' ); ?></div><?php
+		}
 	}
 ?>
