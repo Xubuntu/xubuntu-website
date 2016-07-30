a16a80a23884624bf64610a26788888b09c5465c
Pasi Lallinaho <pasi@shimmerproject.org>
2016-07-31 01:50:35 +0300

    Remove unneeded wrapper from footer (from staging).

--- xubuntu-fifteen/footer.php
+++ xubuntu-fifteen/footer.php
@@ -1,8 +1,6 @@
-<div id="footer_outer" class="group">
-	<div id="footer">
-		<?php get_template_part( 'widgets', 'footer' ); ?>
-		<?php wp_footer( ); ?>
-	</div>
+<div id="footer">
+	<?php get_template_part( 'widgets', 'footer' ); ?>
+	<?php wp_footer( ); ?>
 </div>
 
 </body>
